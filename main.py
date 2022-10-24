b8727575a6cd8cb77c3296b95b85a1b2f07f9a93
malsekri <mohamed.alsekri@gmail.com>
2022-10-24 15:42:38 +0100

    Stephen

---
+++ main.py
@@ -0,0 +1,6 @@
+print("hello lads")
+print((12.4*5.9)/(3.7*2.5)) #Stephen
+print((42.3+18.4)/(16.7*0.4)) #Khalid
+print((81.73-21.4)/(3.87*1.25)) #Khalid
+print(30.5) #Stephen
+print((81.73-21.4)/(3.87*1.25)) #Stephen
